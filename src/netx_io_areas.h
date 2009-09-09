acb3d719bbbbcd0e99aefc80abab7b6503d94d07
doc_bacardi <devnull@localhost>
2009-09-09 15:28:31 +0000

    improved board selection

--- src/netx_io_areas.h
+++ src/netx_io_areas.h
@@ -2,27 +2,26 @@
 #ifndef __NETX_IO_AREAS_H__
 #define __NETX_IO_AREAS_H__
 
-#define DO_CONCAT(a,b) a##b
-#define CONCAT(a,b) DO_CONCAT(a,b)
+#define DO_CONCAT(a,b,c) a##_##b##_##c
+#define CONCAT(a,b,c) DO_CONCAT(a,b,c)
 
 #if defined(__NETX500)
 	#define HOST NX500
-	#define HOSTADR(a)  CONCAT(Adr_NX500_,a)
-	#define HOSTADDR(a) CONCAT(Addr_NX500_,a)
-	#define HOSTMSK(a)  CONCAT(MSK_NX500_,a)
-	#define HOSTSRT(a)  CONCAT(SRT_NX500_,a)
 	#include "netx500/netx500_io_areas.h"
 #else
 #if defined(__NETX50)
 	#define HOST NX50
-	#define HOSTADR(a)  CONCAT(Adr_NX50_,a)
-	#define HOSTADDR(a) CONCAT(Addr_NX50_,a)
-	#define HOSTMSK(a)  CONCAT(MSK_NX50_,a)
-	#define HOSTSRT(a)  CONCAT(SRT_NX50_,a)
 	#include "netx50/netx50_io_areas.h"
 #else
 	#error "no host define set!"
 #endif
 #endif
+
+
+#define HOSTADR(a)  CONCAT(Adr,HOST,a)
+#define HOSTADDR(a) CONCAT(Addr,HOST,a)
+#define HOSTMSK(a)  CONCAT(MSK,HOST,a)
+#define HOSTSRT(a)  CONCAT(SRT,HOST,a)
+
 
 #endif	/* __NETX_IO_AREAS_H__ */
