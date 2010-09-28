7cd04d37413c9c2450a0b0973f62723fa2f8b4ac
doc_bacardi <devnull@localhost>
2010-09-28 13:06:23 +0000

    add more errorchecks

--- testbench/is_erased_parflash.lua
+++ testbench/is_erased_parflash.lua
@@ -109,7 +109,7 @@ else
 
 	-- Use parallel flash on unit 0, chipselect 0.
 	strDevDesc = flasher.detect(tPlugin, aAttr, flasher.BUS_Parflash, 0, 0, ulDevDescAdr)
-	if not strDevDesc then
+	if strDevDesc==nil then
 		error("Failed to get a device description!")
 	end
 
@@ -118,13 +118,18 @@ else
 	ulDataFirst = 0x00020080
 	ulDataEnd = 0x00021000
 	ulEraseFirst,ulEraseEnd = flasher.getEraseArea(tPlugin, aAttr, ulDataFirst, ulDataEnd)
+	if ulEraseFirst==nil or ulEraseEnd==nil then
+		error("Failed to get the erase area!")
+	end
 	print(string.format("Area:  [0x%08x, 0x%08x[", ulDataFirst, ulDataEnd))
 	print(string.format("Erase: [0x%08x, 0x%08x[", ulEraseFirst, ulEraseEnd))
 
 	-- Check if the area is erased.
 	local fIsErased
 	fIsErased = flasher.isErased(tPlugin, aAttr, ulEraseStart, ulEraseEnd)
-	if fIsErased==true then
+	if fIsErased==nil then
+		error("Failed to check if the area is erased!")
+	elseif fIsErased==true then
 		print("!ALL CLEAN! The area is erased.")
 	else
 		print("!!!DIRTY!!! The area must be erased before flashing!")
