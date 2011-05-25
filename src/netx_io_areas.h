72054a89b3ca315a6ae44af6161cd2bff0cf2c99
doc_bacardi <devnull@localhost>
2011-05-25 10:33:06 +0000

    start netx56 support

--- src/netx_io_areas.h
+++ src/netx_io_areas.h
@@ -6,19 +6,23 @@
 #define CONCAT(a,b,c) DO_CONCAT(a,b,c)
 
 #if ASIC_TYP==500
-	#define HOST NX500
-	#define HOSTNAME "netx500"
-	#include "netx500_io_areas.h"
+#       define HOST NX500
+#       define HOSTNAME "netx500"
+#       include "netx500_io_areas.h"
 #elif ASIC_TYP==50
-	#define HOST NX50
-	#define HOSTNAME "netx50"
-	#include "netx50_io_areas.h"
+#       define HOST NX50
+#       define HOSTNAME "netx50"
+#       include "netx50_io_areas.h"
+#elif ASIC_TYP==56
+#       define HOST NX56
+#       define HOSTNAME "netx56"
+#       include "netx56_io_areas.h"
 #elif ASIC_TYP==10
-	#define HOST NX10
-	#define HOSTNAME "netx10"
-	#include "netx10_io_areas.h"
+#       define HOST NX10
+#       define HOSTNAME "netx10"
+#       include "netx10_io_areas.h"
 #else
-	#error "no host define set!"
+#       error "no host define set!"
 #endif
 
 #define HOSTADR(a)  CONCAT(Adr,HOST,a)
@@ -27,4 +31,4 @@
 #define HOSTSRT(a)  CONCAT(SRT,HOST,a)
 #define HOSTADEF(a) CONCAT(HOST,a,AREA_T)
 
-#endif	/* __NETX_IO_AREAS_H__ */
+#endif  /* __NETX_IO_AREAS_H__ */
