647c632b14d0c74d411c5e98d035f6b583c32e72
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2016-12-08 17:07:28 +0100

    Rename Windows_x86 to windows_32 and Windows_x64 to windows_64.

--- .build02_flasher_cli.sh
+++ .build02_flasher_cli.sh
@@ -9,12 +9,12 @@ JONCHKI=/home/cthelen/workspace/org.muhkuh.lua-jonchki/jonchki.lua
 
 # Build the Windows_x86 artifact.
 pushd targets/jonchki/flasher_cli
-lua5.1 ${JONCHKI} -v info --syscfg jonchkisys_Windows_x86.cfg --prjcfg jonchkicfg.xml --target Windows_x86 --finalizer flasher_cli.lua flasher_cli.xml
-pushd Windows_x86/install
-7z a -tzip -mx=9 ../../flasher_cli_windows_x86.zip flasher_cli_windows_x86
+lua5.1 ${JONCHKI} -v info --syscfg jonchkisys_windows_32.cfg --prjcfg jonchkicfg.xml --target windows_32 --finalizer flasher_cli.lua flasher_cli.xml
+pushd windows_32/install
+7z a -tzip -mx=9 ../../flasher_cli_windows_32bit.zip flasher_cli_windows_32bit
 popd
-lua5.1 ${JONCHKI} -v info --syscfg jonchkisys_Windows_x64.cfg --prjcfg jonchkicfg.xml --target Windows_x64 --finalizer flasher_cli.lua flasher_cli.xml
-pushd Windows_x64/install
-7z a -tzip -mx=9 ../../flasher_cli_windows_x64.zip flasher_cli_windows_x64
+lua5.1 ${JONCHKI} -v info --syscfg jonchkisys_windows_64.cfg --prjcfg jonchkicfg.xml --target windows_64 --finalizer flasher_cli.lua flasher_cli.xml
+pushd windows_64/install
+7z a -tzip -mx=9 ../../flasher_cli_windows_64bit.zip flasher_cli_windows_64bit
 popd
 popd
