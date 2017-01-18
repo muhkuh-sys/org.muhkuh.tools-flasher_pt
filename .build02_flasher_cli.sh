b7fd06d464537c52ae74ae8fdf10b5f6e3e343dd
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2017-01-18 11:11:18 +0100

    Use the new Jonchki API to specify the distribution and CPU architecture.

--- .build02_flasher_cli.sh
+++ .build02_flasher_cli.sh
@@ -9,11 +9,11 @@ JONCHKI=/home/cthelen/workspace/org.muhkuh.lua-jonchki/jonchki.lua
 
 # Build the Windows_x86 artifact.
 pushd targets/jonchki/flasher_cli
-lua5.1 ${JONCHKI} -v info --syscfg jonchkisys_windows_32.cfg --prjcfg jonchkicfg.xml --target windows_32 --finalizer flasher_cli.lua flasher_cli.xml
+lua5.1 ${JONCHKI} -v info --syscfg jonchkisys_windows_32.cfg --prjcfg jonchkicfg.xml --distribution-id windows --cpu-architecture x86 --finalizer flasher_cli.lua flasher_cli.xml
 pushd windows_32/install
 7z a -tzip -mx=9 ../../flasher_cli_windows_32bit.zip flasher_cli_windows_32bit
 popd
-lua5.1 ${JONCHKI} -v info --syscfg jonchkisys_windows_64.cfg --prjcfg jonchkicfg.xml --target windows_64 --finalizer flasher_cli.lua flasher_cli.xml
+lua5.1 ${JONCHKI} -v info --syscfg jonchkisys_windows_64.cfg --prjcfg jonchkicfg.xml --distribution-id windows --cpu-architecture x86_64 --finalizer flasher_cli.lua flasher_cli.xml
 pushd windows_64/install
 7z a -tzip -mx=9 ../../flasher_cli_windows_64bit.zip flasher_cli_windows_64bit
 popd
