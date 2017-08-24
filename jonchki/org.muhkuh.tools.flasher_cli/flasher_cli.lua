e6a2c35f2c7ac7a448c2c836b775392fc0cc3cb3
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2017-08-24 18:14:51 +0200

    Use the archive routine from jonchki. Add the version to the archive name. Add a package info file to the release.

--- jonchki/org.muhkuh.tools.flasher_cli/flasher_cli.lua
+++ jonchki/org.muhkuh.tools.flasher_cli/flasher_cli.lua
@@ -1,112 +1,68 @@
 local t = ...
 local strDistId, strDistVersion, strCpuArch = t:get_platform()
+local cLogger = t.cLogger
+local tResult
+local archives = require 'installer.archives'
+local pl = require'pl.import_into'()
 
--- Copy all demo scripts to the installation base.
-t:install('${depack_path_org.muhkuh.tools.flasher.lua5.1-flasher}/demo',  '${install_base}/')
+local atInstall = {
+  -- Copy all demo scripts to the installation base.
+  ['${depack_path_org.muhkuh.tools.flasher.lua5.1-flasher}/demo'] = '${install_base}/',
 
--- Create a ZIP archive for Windows and a TAR.GZ for Linux.
-if strDistId=='windows' then
-elseif strDistId=='ubuntu' then
+  -- Copy the report.
+  ['${report_path}']                                              = '${install_base}/.jonchki/',
+  ['${report_xslt}']                                              = '${install_base}/.jonchki/'
+}
+for strSrc, strDst in pairs(atInstall) do
+  t:install(strSrc, strDst)
 end
 
--- Loop over all files in the install folder.
-local archive = require 'archive'
 
--- Create a new archive.
-local tArchive = archive.ArchiveWrite()
-
--- Create a read disk object.
-local tReader = archive.ArchiveReadDisk()
-
-local strExtension
-if strDistId=='windows' then
-  strExtension = 'zip'
-
-  tResult = tArchive:set_format_zip()
-  if tResult~=0 then
-    error('Failed to set the archive format to ZIP.')
-  end
-elseif strDistId=='ubuntu' then
-  strExtension = 'tar.gz'
-
-  tResult = tArchive:set_format_gnutar()
-  if tResult~=0 then
-    error('Failed to set the archive format to GNU TAR.')
-  end
-  tResult = tArchive:add_filter_gzip()
-  if tResult~=0 then
-    error('Failed to add GZIP filter.')
-  end
+-- Create the package file.
+local strPackageText = t:replace_template([[PACKAGE_NAME=${root_artifact_artifact}
+PACKAGE_VERSION=${root_artifact_version}
+PACKAGE_VCS_ID=${root_artifact_vcs_id}
+HOST_DISTRIBUTION_ID=${platform_distribution_id}
+HOST_DISTRIBUTION_VERSION=${platform_distribution_version}
+HOST_CPU_ARCHITECTURE=${platform_cpu_architecture}
+]])
+local strPackagePath = t:replace_template('${install_base}/.jonchki/package.txt')
+local tFileError, strError = pl.utils.writefile(strPackagePath, strPackageText, false)
+if tFileError==nil then
+  cLogger:error('Failed to write the package file "%s": %s', strPackagePath, strError)
 else
-  error('Unknown distribution ID:' .. tostring(strDistId))
-end
+  local Archive = archives(cLogger)
 
--- Translate the CPU architecture to the bits.
-local strCpuBits
-if strCpuArch=='x86' then
-  strCpuBits = '32'
-elseif strCpuArch=='x86_64' then
-  strCpuBits = '64'
-else
-  error('Unknown CPU architecture:' .. tostring(strCpuArch))
-end
-
-local strArtifact = string.format('flasher_cli_%s%s_%sbit', strDistId, strDistVersion, strCpuBits)
-local strArchive = t:replace_template(string.format('${install_base}/../%s.%s', strArtifact, strExtension))
-local strInstallFolder = t:replace_template('${install_base}')
-local strArchivePrefix = strArtifact
-
-local strArchiveRoot = t.pl.path.abspath(strInstallFolder)
-local tResult = tReader:open(strArchiveRoot)
-if tResult~=0 then
-  error(string.format('Failed to open the local folder "%s" for reading.', strArchiveRoot))
-end
-
--- Remove the old archive if it exists.
-if t.pl.path.exists(strArchive)==strArchive then
-  t.pl.file.delete(strArchive)
-end
-
-tResult = tArchive:open_filename(strArchive)
-if tResult~=0 then
-  error(string.format('Failed to open archive "%s" for writing.', strArchive))
-end
-
-for tEntry in tReader:iter_header() do
-  -- Cut off the archive root.
-  local strAbsEntry = tEntry:pathname()
-  local strRelEntry = t.pl.path.join(strArchivePrefix, t.pl.path.relpath(strAbsEntry, strArchiveRoot))
-  if strRelEntry~='' then
-    tEntry:set_pathname(strRelEntry)
-    tResult = tArchive:write_header(tEntry)
-    if tResult~=0 then
-      error(string.format('Failed to write the header for entry "%s".', strAbsEntry))
-    end
-    -- Copy the data.
-    for strData in tReader:iter_data(16384) do
-      tResult = tArchive:write_data(strData)
-      if tResult~=0 then
-        error(string.format('Failed to write data for entry "%s".', strAbsEntry))
-      end
-    end
-    -- Finish the entry.
-    tResult = tArchive:finish_entry()
-    if tResult~=0 then
-      error(string.format('Failed to finish the entry "%s".', strAbsEntry))
-    end
+  -- Create a ZIP archive for Windows platforms. Build a "tar.gz" for Linux.
+  local strArchiveExtension
+  local tFormat
+  local atFilter
+  if strDistId=='windows' then
+    strArchiveExtension = 'zip'
+    tFormat = Archive.archive.ARCHIVE_FORMAT_ZIP
+    atFilter = {}
+  else
+    strArchiveExtension = 'tar.gz'
+    tFormat = Archive.archive.ARCHIVE_FORMAT_TAR_GNUTAR
+    atFilter = { Archive.archive.ARCHIVE_FILTER_GZIP }
   end
-  if tReader:can_descend()~=0 then
-    tResult = tReader:descend()
-    if tResult~=0 then
-      error(string.format('Failed to descend into entry "%s".', strAbsEntry))
-    end
+
+  -- Translate the CPU architecture to bits.
+  local atCpuArchToBits = {
+    ['x86'] = 32,
+    ['x86_64'] = 64
+  }
+  local uiPlatformBits = atCpuArchToBits[strCpuArch]
+  if uiPlatformBits==nil then
+    cLogger:error('Failed to translate the CPU architecture "%s" to bits.', strCpuArch)
+  else
+    local strArtifactVersion = t:replace_template('${root_artifact_artifact}-${root_artifact_version}')
+    local strArchive = t:replace_template(string.format('${install_base}/../%s-%s%s_%dbit.%s', strArtifactVersion, strDistId, strDistVersion, uiPlatformBits, strArchiveExtension))
+    local strDiskPath = t:replace_template('${install_base}')
+    local strArchiveMemberPrefix = strArtifactVersion
+
+    tResult = Archive:pack_archive(strArchive, tFormat, atFilter, strDiskPath, strArchiveMemberPrefix)
   end
 end
 
-tResult = tArchive:close()
-if tResult~=0 then
-  error(string.format('Failed to close the archive "%s".', strArchive))
-end
-tReader:close()
-
-return true
+return tResult
