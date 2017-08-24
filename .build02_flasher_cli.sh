f155fe16048be627fbafe9722e882924b56c66a6
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2017-08-24 18:13:57 +0200

    Use jonchki in /tmp. Clean up the script.

--- .build02_flasher_cli.sh
+++ .build02_flasher_cli.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
 set -e
 
+VERBOSITY=debug
+
 # Get the project folder.
 PRJ_DIR=`pwd`
 
 # This is the path to the jonchki tool.
-JONCHKI_PATH=/tmp/jonchki/install
-JONCHKI=${JONCHKI_PATH}/jonchki.lua
+JONCHKI=/tmp/jonchki-1.0.0.1/jonchki
 
 # This is the base path to the jonchki working folders.
 WORK_BASE=${PRJ_DIR}/targets/jonchki/flasher_cli
@@ -30,38 +31,40 @@ mkdir -p ${WORK_BASE}/ubuntu_16.04_64bit
 mkdir -p ${WORK_BASE}/ubuntu_17.04_32bit
 mkdir -p ${WORK_BASE}/ubuntu_17.04_64bit
 
+COMMON_OPTIONS="--syscfg ${PRJ_DIR}/jonchki/org.muhkuh.tools.flasher_cli/jonchkisys.cfg --prjcfg ${PRJ_DIR}/jonchki/org.muhkuh.tools.flasher_cli/jonchkicfg.xml --finalizer ${PRJ_DIR}/jonchki/org.muhkuh.tools.flasher_cli/flasher_cli.lua ${WORK_BASE}/flasher_cli.xml"
+
 # Build the Windows_x86 artifact.
 pushd ${WORK_BASE}/windows_32bit
-LD_LIBRARY_PATH=${JONCHKI_PATH} ${JONCHKI_PATH}/lua5.1 ${JONCHKI} -v debug --syscfg ${PRJ_DIR}/jonchki/org.muhkuh.tools.flasher_cli/jonchkisys.cfg --prjcfg ${PRJ_DIR}/jonchki/org.muhkuh.tools.flasher_cli/jonchkicfg.xml --distribution-id windows --distribution-version "" --cpu-architecture x86 --finalizer ${PRJ_DIR}/jonchki/org.muhkuh.tools.flasher_cli/flasher_cli.lua ${WORK_BASE}/flasher_cli.xml
+${JONCHKI} -v ${VERBOSITY} --distribution-id windows --empty-distribution-version --cpu-architecture x86 ${COMMON_OPTIONS}
 popd
 # Build the Windows_x86_64 artifact.
 pushd ${WORK_BASE}/windows_64bit
-LD_LIBRARY_PATH=${JONCHKI_PATH} ${JONCHKI_PATH}/lua5.1 ${JONCHKI} -v info --syscfg ${PRJ_DIR}/jonchki/org.muhkuh.tools.flasher_cli/jonchkisys.cfg --prjcfg ${PRJ_DIR}/jonchki/org.muhkuh.tools.flasher_cli/jonchkicfg.xml --distribution-id windows --distribution-version "" --cpu-architecture x86_64 --finalizer ${PRJ_DIR}/jonchki/org.muhkuh.tools.flasher_cli/flasher_cli.lua ${WORK_BASE}/flasher_cli.xml
+${JONCHKI} -v ${VERBOSITY} --distribution-id windows --empty-distribution-version --cpu-architecture x86_64 ${COMMON_OPTIONS}
 popd
 
 # Ubuntu 14.04 32bit
 pushd ${WORK_BASE}/ubuntu_14.04_32bit
-LD_LIBRARY_PATH=${JONCHKI_PATH} ${JONCHKI_PATH}/lua5.1 ${JONCHKI} -v info --syscfg ${PRJ_DIR}/jonchki/org.muhkuh.tools.flasher_cli/jonchkisys.cfg --prjcfg ${PRJ_DIR}/jonchki/org.muhkuh.tools.flasher_cli/jonchkicfg.xml --distribution-id ubuntu --distribution-version 14.04 --cpu-architecture x86 --finalizer ${PRJ_DIR}/jonchki/org.muhkuh.tools.flasher_cli/flasher_cli.lua ${WORK_BASE}/flasher_cli.xml
+${JONCHKI} -v ${VERBOSITY} --distribution-id ubuntu --distribution-version 14.04 --cpu-architecture x86 ${COMMON_OPTIONS}
 popd
 # Ubuntu 14.04 64bit
 pushd ${WORK_BASE}/ubuntu_14.04_64bit
-LD_LIBRARY_PATH=${JONCHKI_PATH} ${JONCHKI_PATH}/lua5.1 ${JONCHKI} -v info --syscfg ${PRJ_DIR}/jonchki/org.muhkuh.tools.flasher_cli/jonchkisys.cfg --prjcfg ${PRJ_DIR}/jonchki/org.muhkuh.tools.flasher_cli/jonchkicfg.xml --distribution-id ubuntu --distribution-version 14.04 --cpu-architecture x86_64 --finalizer ${PRJ_DIR}/jonchki/org.muhkuh.tools.flasher_cli/flasher_cli.lua ${WORK_BASE}/flasher_cli.xml
+${JONCHKI} -v ${VERBOSITY} --distribution-id ubuntu --distribution-version 14.04 --cpu-architecture x86_64 ${COMMON_OPTIONS}
 popd
 
 # Ubuntu 16.04 32bit
 pushd ${WORK_BASE}/ubuntu_16.04_32bit
-LD_LIBRARY_PATH=${JONCHKI_PATH} ${JONCHKI_PATH}/lua5.1 ${JONCHKI} -v info --syscfg ${PRJ_DIR}/jonchki/org.muhkuh.tools.flasher_cli/jonchkisys.cfg --prjcfg ${PRJ_DIR}/jonchki/org.muhkuh.tools.flasher_cli/jonchkicfg.xml --distribution-id ubuntu --distribution-version 16.04 --cpu-architecture x86 --finalizer ${PRJ_DIR}/jonchki/org.muhkuh.tools.flasher_cli/flasher_cli.lua ${WORK_BASE}/flasher_cli.xml
+${JONCHKI} -v ${VERBOSITY} --distribution-id ubuntu --distribution-version 16.04 --cpu-architecture x86 ${COMMON_OPTIONS}
 popd
 # Ubuntu 16.04 64bit
 pushd ${WORK_BASE}/ubuntu_16.04_64bit
-LD_LIBRARY_PATH=${JONCHKI_PATH} ${JONCHKI_PATH}/lua5.1 ${JONCHKI} -v info --syscfg ${PRJ_DIR}/jonchki/org.muhkuh.tools.flasher_cli/jonchkisys.cfg --prjcfg ${PRJ_DIR}/jonchki/org.muhkuh.tools.flasher_cli/jonchkicfg.xml --distribution-id ubuntu --distribution-version 16.04 --cpu-architecture x86_64 --finalizer ${PRJ_DIR}/jonchki/org.muhkuh.tools.flasher_cli/flasher_cli.lua ${WORK_BASE}/flasher_cli.xml
+${JONCHKI} -v ${VERBOSITY} --distribution-id ubuntu --distribution-version 16.04 --cpu-architecture x86_64 ${COMMON_OPTIONS}
 popd
 
 # Ubuntu 17.04 32bit
 pushd ${WORK_BASE}/ubuntu_17.04_32bit
-LD_LIBRARY_PATH=${JONCHKI_PATH} ${JONCHKI_PATH}/lua5.1 ${JONCHKI} -v info --syscfg ${PRJ_DIR}/jonchki/org.muhkuh.tools.flasher_cli/jonchkisys.cfg --prjcfg ${PRJ_DIR}/jonchki/org.muhkuh.tools.flasher_cli/jonchkicfg.xml --distribution-id ubuntu --distribution-version 17.04 --cpu-architecture x86 --finalizer ${PRJ_DIR}/jonchki/org.muhkuh.tools.flasher_cli/flasher_cli.lua ${WORK_BASE}/flasher_cli.xml
+${JONCHKI} -v ${VERBOSITY} --distribution-id ubuntu --distribution-version 17.04 --cpu-architecture x86 ${COMMON_OPTIONS}
 popd
 # Ubuntu 17.04 64bit
 pushd ${WORK_BASE}/ubuntu_17.04_64bit
-LD_LIBRARY_PATH=${JONCHKI_PATH} ${JONCHKI_PATH}/lua5.1 ${JONCHKI} -v info --syscfg ${PRJ_DIR}/jonchki/org.muhkuh.tools.flasher_cli/jonchkisys.cfg --prjcfg ${PRJ_DIR}/jonchki/org.muhkuh.tools.flasher_cli/jonchkicfg.xml --distribution-id ubuntu --distribution-version 17.04 --cpu-architecture x86_64 --finalizer ${PRJ_DIR}/jonchki/org.muhkuh.tools.flasher_cli/flasher_cli.lua ${WORK_BASE}/flasher_cli.xml
+${JONCHKI} -v ${VERBOSITY} --distribution-id ubuntu --distribution-version 17.04 --cpu-architecture x86_64 ${COMMON_OPTIONS}
 popd
