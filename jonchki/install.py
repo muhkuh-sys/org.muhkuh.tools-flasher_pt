ee0b041acc8f8352e64bd3490911289bbfe10e1a
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2019-12-17 13:29:04 +0100

    Add the jonchki install helper.

---
+++ jonchki/install.py
@@ -0,0 +1,32 @@
+import os
+import subprocess
+
+
+def install_host_debs(astrDeb):
+    astrInstall = []
+    for strDeb in astrDeb:
+        strDpkgStatus = subprocess.check_output("dpkg-query -W -f='${Status}' %s || echo 'unknown'" % strDeb, shell=True)
+        print('Check for %s = %s' % (strDeb, strDpkgStatus))
+        if strDpkgStatus != 'install ok installed':
+            astrInstall.append(strDeb)
+    if len(astrInstall) != 0:
+        subprocess.check_call('sudo apt-get update --assume-yes', shell=True)
+        subprocess.check_call('sudo apt-get install --assume-yes %s' % ' '.join(astrInstall), shell=True)
+
+
+def install_foreign_debs(astrDeb, strCfg_workingFolder, strCfg_projectFolder):
+    # Create the folders if they do not exist yet.
+    astrFolders = [
+        strCfg_workingFolder,
+        os.path.join(strCfg_workingFolder, 'packages'),
+    ]
+    for strPath in astrFolders:
+        if os.path.exists(strPath) is not True:
+            os.makedirs(strPath)
+
+    packagesPath = os.path.join(strCfg_workingFolder, 'packages')
+    os.chdir(packagesPath)
+    subProcessPath = os.path.join(strCfg_projectFolder, 'cmake', 'tools')
+    subProcessCall = '%s/get_dependencies.sh %s' % (subProcessPath, ' '.join(astrDeb))
+    subprocess.check_call(subProcessCall, shell=True)
+    os.chdir(strCfg_workingFolder)
