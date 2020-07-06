f297c7453b760147bf3ee2aa4a3277ff98f7aa0d
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2020-07-06 10:32:48 +0200

    Modifications of LD_LIBRARY_PATH became obsolete with LUA5.1.5.8 .

--- jonchki/org.muhkuh.tools.flasher_cli/finalizer.lua
+++ jonchki/org.muhkuh.tools.flasher_cli/finalizer.lua
@@ -19,8 +19,6 @@ end
 
 -- Install the wrapper.
 if strDistId=='ubuntu' then
-  -- This is a shell script setting the library search path for the LUA shared object.
-  t:install('../../wrapper/linux/lua5.1.sh', '${install_base}/')
   -- Copy the muhkuh CLI init for linux.
   t:install('../../jonchki/org.muhkuh.tools.flasher_cli/linux/muhkuh_cli_init.lua', '${install_base}/')
 elseif strDistId=='windows' then
