ea56eee0c601b0546152fc1311890b90f098a0d3
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2020-07-06 09:33:19 +0200

    Build SWFP files.

--- lua/wfp.lua
+++ lua/wfp.lua
@@ -5,6 +5,17 @@ local argparse = require 'argparse'
 local wfp_control = require 'wfp_control'
 
 
+local function writeU32(tFile, ulData)
+  local ucB0 = math.fmod(ulData, 256)
+  ulData = (ulData - ucB0) / 256
+  local ucB1 = math.fmod(ulData, 256)
+  ulData = (ulData - ucB1) / 256
+  local ucB2 = math.fmod(ulData, 256)
+  local ucB3 = (ulData - ucB2) / 256
+  tFile:write(string.char(ucB0, ucB1, ucB2, ucB3))
+end
+
+
 local strFlasherPrefix = 'netx/'
 
 local atLogLevels = {
@@ -55,6 +66,10 @@ tParserCommandPack:flag('-o --overwrite')
   :description('Overwrite an existing WFP archive. The default is to do nothing if the target archive already exists.')
   :default(false)
   :target('fOverwrite')
+tParserCommandPack:flag('-s --simple')
+  :description('Build a SWFP file without compression.')
+  :default(false)
+  :target('fBuildSWFP')
 tParserCommandPack:option('-v --verbose')
   :description(string.format('Set the verbosity level to LEVEL. Possible values for LEVEL are %s.', table.concat(atLogLevels, ', ')))
   :argname('<LEVEL>')
@@ -77,7 +92,8 @@ local tLog = require 'log'.new(
 local atName2Bus = {
   ['Parflash'] = flasher.BUS_Parflash,
   ['Spi']      = flasher.BUS_Spi,
-  ['IFlash']   = flasher.BUS_IFlash
+  ['IFlash']   = flasher.BUS_IFlash,
+  ['SDIO']     = flasher.BUS_SDIO
 }
 
 
@@ -240,29 +256,44 @@ elseif tArgs.fCommandPackSelected==true then
       local atSortedFiles = {}
       for strTarget, tTarget in pairs(tWfpControl.atConfigurationTargets) do
         for _, tTargetFlash in ipairs(tTarget.atFlashes) do
-          for _, tData in ipairs(tTargetFlash.atData) do
-            local strFile = tData.strFile
-            local strFileAbs = strFile
-            if pl.path.isabs(strFileAbs)~=true then
-              strFileAbs = pl.path.join(strWorkingPath, strFileAbs)
-              tLog.debug('Extending the relative path "%s" to "%s".', strFile, strFileAbs)
-            end
-            local strFileBase = pl.path.basename(strFile)
-            if atFiles[strFileBase]==nil then
-              if pl.path.exists(strFileAbs)~=strFileAbs then
-                tLog.error('The path "%s" does not exist.', strFileAbs)
-                fOk = false
-              elseif pl.path.isfile(strFileAbs)~=true then
-                tLog.error('The path "%s" does not point to a file.', strFileAbs)
-                fOk = false
-              else
-                tLog.debug('Adding file "%s" to the list.', strFileAbs)
-                atFiles[strFileBase] = strFileAbs
-                table.insert(atSortedFiles, strFileAbs)
+          local strBusName = tTargetFlash.strBus
+          local tBus = atName2Bus[strBusName]
+          if tBus==nil then
+            tLog.error('Unknown bus "%s" found in WFP control file.', strBusName)
+            fOk = false
+            break
+          else
+            for _, tData in ipairs(tTargetFlash.atData) do
+              local strFile = tData.strFile
+              local strFileAbs = strFile
+              if pl.path.isabs(strFileAbs)~=true then
+                strFileAbs = pl.path.join(strWorkingPath, strFileAbs)
+                tLog.debug('Extending the relative path "%s" to "%s".', strFile, strFileAbs)
               end
-            elseif atFiles[strFileBase]~=strFileAbs then
-              tLog.error('Multiple files with the base name "%s" found.', strFileBase)
-              fOk = false
+              local strFileBase = pl.path.basename(strFile)
+              if atFiles[strFileBase]==nil then
+                if pl.path.exists(strFileAbs)~=strFileAbs then
+                  tLog.error('The path "%s" does not exist.', strFileAbs)
+                  fOk = false
+                elseif pl.path.isfile(strFileAbs)~=true then
+                  tLog.error('The path "%s" does not point to a file.', strFileAbs)
+                  fOk = false
+                else
+                  tLog.debug('Adding file "%s" to the list.', strFileAbs)
+                  atFiles[strFileBase] = strFileAbs
+                  local tAttr = {
+                    ucBus = tBus,
+                    ucUnit = tTargetFlash.ulUnit,
+                    ucChipSelect = tTargetFlash.ulChipSelect,
+                    ulOffset = tData.ulOffset,
+                    strFilename = strFileAbs
+                  }
+                  table.insert(atSortedFiles, tAttr)
+                end
+              elseif atFiles[strFileBase]~=strFileAbs then
+                tLog.error('Multiple files with the base name "%s" found.', strFileBase)
+                fOk = false
+              end
             end
           end
         end
@@ -270,48 +301,36 @@ elseif tArgs.fCommandPackSelected==true then
       if fOk~=true then
         tLog.error('Not all files are OK. Stopping here.')
       else
-        -- Create a new archive.
-        local tArchive = archive.ArchiveWrite()
-        local tFormat = archive.ARCHIVE_FORMAT_TAR_GNUTAR
-        tArcResult = tArchive:set_format(tFormat)
-        if tArcResult~=0 then
-          tLog.error('Failed to set the archive format to ID %d: %s', tFormat, tArchive:error_string())
-          fOk = false
-        else
-          local atFilter = { archive.ARCHIVE_FILTER_XZ }
-          for _, tFilter in ipairs(atFilter) do
-            tArcResult = tArchive:add_filter(tFilter)
-            if tArcResult~=0 then
-              tLog.error('Failed to add filter with ID %d: %s', tFilter, tArchive:error_string())
-              fOk = false
-              break
-            end
-          end
-
-          local tTimeNow = os.time()
-          local strWfpArchiveFile = tArgs.strWfpArchiveFile
-          tArcResult = tArchive:open_filename(strWfpArchiveFile)
+        if tArgs.fBuildSWFP==false then
+          -- Create a new archive.
+          local tArchive = archive.ArchiveWrite()
+          local tFormat = archive.ARCHIVE_FORMAT_TAR_GNUTAR
+          tArcResult = tArchive:set_format(tFormat)
           if tArcResult~=0 then
-            tLog.error('Failed to open the archive "%s": %s', strWfpArchiveFile, tArchive:error_string())
+            tLog.error('Failed to set the archive format to ID %d: %s', tFormat, tArchive:error_string())
             fOk = false
           else
-            -- Add the control file.
-            local tEntry = archive.ArchiveEntry()
-            tEntry:set_pathname('wfp.xml')
-            local strData = pl.utils.readfile(tArgs.strWfpControlFile, true)
-            tEntry:set_size(string.len(strData))
-            tEntry:set_filetype(archive.AE_IFREG)
-            tEntry:set_perm(420)
-            tEntry:set_gname('wfp')
---            tEntry:set_uname('wfp')
-            tArchive:write_header(tEntry)
-            tArchive:write_data(strData)
-            tArchive:finish_entry()
+            local atFilter = { archive.ARCHIVE_FILTER_XZ }
+            for _, tFilter in ipairs(atFilter) do
+              tArcResult = tArchive:add_filter(tFilter)
+              if tArcResult~=0 then
+                tLog.error('Failed to add filter with ID %d: %s', tFilter, tArchive:error_string())
+                fOk = false
+                break
+              end
+            end
 
-            for _, strFileAbs in ipairs(atSortedFiles) do
+            local tTimeNow = os.time()
+            local strWfpArchiveFile = tArgs.strWfpArchiveFile
+            tArcResult = tArchive:open_filename(strWfpArchiveFile)
+            if tArcResult~=0 then
+              tLog.error('Failed to open the archive "%s": %s', strWfpArchiveFile, tArchive:error_string())
+              fOk = false
+            else
+              -- Add the control file.
               local tEntry = archive.ArchiveEntry()
-              tEntry:set_pathname(pl.path.basename(strFileAbs))
-              local strData = pl.utils.readfile(strFileAbs, true)
+              tEntry:set_pathname('wfp.xml')
+              local strData = pl.utils.readfile(tArgs.strWfpControlFile, true)
               tEntry:set_size(string.len(strData))
               tEntry:set_filetype(archive.AE_IFREG)
               tEntry:set_perm(420)
@@ -320,10 +339,54 @@ elseif tArgs.fCommandPackSelected==true then
               tArchive:write_header(tEntry)
               tArchive:write_data(strData)
               tArchive:finish_entry()
-            end
-          end
 
-          tArchive:close()
+              for _, tAttr in ipairs(atSortedFiles) do
+                local tEntry = archive.ArchiveEntry()
+                tEntry:set_pathname(pl.path.basename(tAttr.strFilename))
+                local strData = pl.utils.readfile(tAttr.strFilename, true)
+                tEntry:set_size(string.len(strData))
+                tEntry:set_filetype(archive.AE_IFREG)
+                tEntry:set_perm(420)
+                tEntry:set_gname('wfp')
+--                tEntry:set_uname('wfp')
+                tArchive:write_header(tEntry)
+                tArchive:write_data(strData)
+                tArchive:finish_entry()
+              end
+            end
+
+            tArchive:close()
+          end
+        else
+          -- Build a SWFP.
+
+          -- Create the new archive.
+          local tArchive, strError = io.open(tArgs.strWfpArchiveFile, 'wb')
+          if tArchive==nil then
+            tLog.error('Failed to create the new SWFP archive "%s": %s', tArgs.strWfpArchiveFile, strError)
+            fOk = false
+          else
+            -- Write the SWFP magic.
+            tArchive:write(string.char(0x53, 0x57, 0x46, 0x50))
+
+            -- Loop over all files.
+            for _, tAttr in ipairs(atSortedFiles) do
+              -- Get the file data.
+              local strData = pl.utils.readfile(tAttr.strFilename, true)
+
+              -- Write the chunk header.
+              tArchive:write(string.char(tAttr.ucBus))
+              tArchive:write(string.char(tAttr.ucUnit))
+              tArchive:write(string.char(tAttr.ucChipSelect))
+              writeU32(tArchive, tAttr.ulOffset)
+              writeU32(tArchive, string.len(strData))
+
+              -- Write the data.
+              tArchive:write(strData)
+            end
+
+            tArchive:close()
+          end
         end
       end
     end
