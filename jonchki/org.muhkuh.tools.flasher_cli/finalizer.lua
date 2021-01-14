a9fc0e1b895bdba47eeb8a298c283e34d7e06671
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2021-01-14 13:14:26 +0100

    Move all command scripts except wfp to the direct API examples.

--- jonchki/org.muhkuh.tools.flasher_cli/finalizer.lua
+++ jonchki/org.muhkuh.tools.flasher_cli/finalizer.lua
@@ -10,8 +10,11 @@ local atInstall = {
   -- Copy the muhkuh cli init script. The active plugins are defined here.
   ['muhkuh_cli_init.lua'] = '${install_base}/',
 
-  -- Copy all demo scripts to the installation base.
-  ['${depack_path_org.muhkuh.tools.flasher_pt.flasher}/demo'] = '${install_base}/',
+  -- Copy all tools to the installation base.
+  ['${depack_path_org.muhkuh.tools.flasher_pt.flasher}/tools'] = '${install_base}/',
+
+  -- Copy all API demos.
+  ['${depack_path_org.muhkuh.tools.flasher_pt.flasher}/direct_api_examples'] = '${install_base}/direct_api_examples/',
 
   -- Copy the report.
   ['${report_path}'] = '${install_base}/.jonchki/'
